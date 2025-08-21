2b2fd3e1c1dffb6f26b0f1d2926bab1e6bdabab4
gvadiko2004 <archilbigvava@gmail.com>
2025-08-21 11:46:29 +0300

    js  correct

--- js/main.js
+++ js/main.js
@@ -16,3 +16,15 @@ linkMenu.forEach((item) => {
     menu.classList.remove("active");
   });
 });
+
+
+
+const header = document.querySelector('.header');
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        header.classList.add('active');
+      } else {
+        header.classList.remove('active');
+      }
+    });
